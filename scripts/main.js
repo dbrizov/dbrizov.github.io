2010737c9d9d2f605c297bd35046df52bb7cd228
dbrizov <d.b.rizov@gmail.com>
2025-11-09 22:53:34 +0200

    Display everything on the landing page

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,16 @@
 	}
 	
 	function loadAbout() {
-		loadContent("https://raw.githubusercontent.com/dbrizov/dbrizov.github.io/master/html/about.html", "#main_content");
+		loadContent("https://raw.githubusercontent.com/dbrizov/dbrizov.github.io/master/html/about.html", "#about");
 	}
 	
 	function loadProjects() {
-		loadContent("https://raw.githubusercontent.com/dbrizov/dbrizov.github.io/master/html/projects.html", "#main_content");
+		loadContent("https://raw.githubusercontent.com/dbrizov/dbrizov.github.io/master/html/projects.html", "#projects");
 	}
 	
-	$("#btn-about").click(loadAbout);
-	$("#btn-projects").click(loadProjects);
+	// $("#btn-about").click(loadAbout);
+	// $("#btn-projects").click(loadProjects);
 	
 	loadAbout();
+	loadProjects();
 }());
